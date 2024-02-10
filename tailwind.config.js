f55793fc6b0b8956e06f24daccfaa26f833231d5
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-02-10 18:37:04 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#E74C3C",
+          secondary: "#141618",
+          neutral: "#ffff",
+          "base-100": "#ffff",
+          info: "#ECF0F3",
+          success: "#ffff",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
